b7987f37c6387cb6189ed5a6a418fa24e9ce171a
Ira <ira.remiracle@gmail.com>
2021-11-23 11:48:05 +0700

    first commit

---
+++ database/migrations/2021_11_12_103520_create_transactions.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactions extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('book_id');
+            $table->unsignedInteger('user_id');
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
